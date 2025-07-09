e488f39818503850775bad1b8cade817960c55c3
Tesfalegnp <peterhope935@example.com>
2025-07-09 22:22:22 +0300

    perf: convert enemy update loop to struct-of-arrays

---
+++ dop_struct_of_arrays/StructTest.cpp
@@ -0,0 +1,20 @@
+// dop_struct_of_arrays/StructTest.cpp
+#include "../include/common.hpp"
+
+struct Enemies {
+    std::vector<float> x, y, vx, vy;
+};
+
+void run_struct_test(int num_entities) {
+    Enemies enemies;
+    enemies.x.resize(num_entities, 0.0f);
+    enemies.y.resize(num_entities, 0.0f);
+    enemies.vx.resize(num_entities, 1.0f);
+    enemies.vy.resize(num_entities, 1.0f);
+
+    Timer timer("StructTest");
+    for (int i = 0; i < num_entities; ++i) {
+        enemies.x[i] += enemies.vx[i];
+        enemies.y[i] += enemies.vy[i];
+    }
+}
